9088df102164c7d9499976f919e651c9e04a35ad
Sai Deepak <saideepak1914@gmail.com>
2024-06-24 17:01:19 +0530

    Initial setup

---
+++ todo_list/base/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True,
+                             blank=True)
+    name = models.CharField(max_length=200, null=True , blank=True)
+    description = models.TextField(null=True , blank=True)
+    complete = models.BooleanField(default=False)
+    deadline = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        if self.name:
+            return self.name
+        else:
+            return f'Task-{self.id}'
+
+    class Meta:
+        ordering = ['complete']
